b759957b27271945dc9072c0b0d8197158d675c1
Till Bungert <tillbungert@gmail.com>
2026-05-28 22:43:24 +0200

    chore: initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from transformers import AutoProcessor, AutoModelForImageTextToText
+
+
+def main():
+    processor = AutoProcessor.from_pretrained("Qwen/Qwen3.5-0.8B")
+    model = AutoModelForImageTextToText.from_pretrained("Qwen/Qwen3.5-0.8B")
+    messages = [
+        {
+            "role": "user",
+            "content": [{"type": "text", "text": "Hello, what's your name."}],
+        },
+    ]
+    inputs = processor.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        tokenize=True,
+        return_dict=True,
+        return_tensors="pt",
+    ).to(model.device)
+
+    outputs = model.generate(**inputs, max_new_tokens=40)
+    print(processor.decode(outputs[0][inputs["input_ids"].shape[-1] :]))
+
+
+if __name__ == "__main__":
+    main()
