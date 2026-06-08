473d4f37c4b9e2d0b19aeb049550f0f6868140b3
Till Bungert <tillbungert@gmail.com>
2026-06-08 23:12:41 +0200

    fix: try qwen 2.5

--- main.py
+++ main.py
@@ -3,18 +3,29 @@ import time
 import torch
 from itertools import batched
 from datasets import load_dataset
-from transformers import AutoModelForImageTextToText, AutoProcessor, DynamicCache
+from transformers import AutoModelForImageTextToText, AutoProcessor, DynamicCache, AutoModelForCausalLM
 from transformers.cache_utils import CacheLayerMixin, LinearAttentionCacheLayerMixin
 from query_aware_cache import QueryAwareCache, bind_query_aware_cache
+from safetensors.torch import save_file
 
 
 def main():
     # processor = AutoProcessor.from_pretrained("Qwen/Qwen3.5-0.8B")
-    # model = AutoModelForImageTextToText.from_pretrained("Qwen/Qwen3.5-0.8B").eval().to("cuda")
-    processor = AutoProcessor.from_pretrained("Qwen/Qwen3.5-9B")
+    # model = AutoModelForImageTextToText.from_pretrained("Qwen/Qwen3.5-0.8B").eval().to("mps")
+    # processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-0.5B")
+    # model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-0.5B").eval().to("mps")
+    # processor = AutoProcessor.from_pretrained("Qwen/Qwen3.5-9B")
+    # model = (
+    #     AutoModelForImageTextToText.from_pretrained(
+    #         "Qwen/Qwen3.5-9B", attn_implementation="sdpa", dtype=torch.bfloat16
+    #     )
+    #     .eval()
+    #     .to("cuda")
+    # )
+    processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-7B")
     model = (
-        AutoModelForImageTextToText.from_pretrained(
-            "Qwen/Qwen3.5-9B", attn_implementation="sdpa", dtype=torch.bfloat16
+        AutoModelForCausalLM.from_pretrained(
+            "Qwen/Qwen2.5-7B", attn_implementation="sdpa", dtype=torch.bfloat16
         )
         .eval()
         .to("cuda")
@@ -34,7 +45,8 @@ def main():
         messages = [
             {
                 "role": "user",
-                "content": [{"type": "text", "text": context}],
+                # "content": [{"type": "text", "text": context}],
+                "content": context,
             } for context in batches[i]
         ]
         i += 1
@@ -51,13 +63,16 @@ def main():
         print(f"{i=} {total=}")
         input_chunks = torch.split(inputs["input_ids"], 1024, -1)
         attention_masks = torch.split(inputs["attention_mask"], 1024, -1)
-        mm_token_type_chunks = torch.split(inputs["mm_token_type_ids"], 1024, -1)
-        for input_ids, attention_mask, mm_token_type_ids in zip(
-            input_chunks, attention_masks, mm_token_type_chunks
+        # mm_token_type_chunks = torch.split(inputs["mm_token_type_ids"], 1024, -1)
+        # for input_ids, attention_mask, mm_token_type_ids in zip(
+        #     input_chunks, attention_masks, mm_token_type_chunks
+        # ):
+        for input_ids, attention_mask in zip(
+            input_chunks, attention_masks
         ):
             input_ids = input_ids.to(model.device)
             attention_mask = attention_mask.to(model.device)
-            mm_token_type_ids = mm_token_type_ids.to(model.device)
+            # mm_token_type_ids = mm_token_type_ids.to(model.device)
             with torch.no_grad():
                 with torch.nn.attention.sdpa_kernel(
                     [
@@ -68,18 +83,20 @@ def main():
                     past_key_values = model(
                         input_ids=input_ids,
                         attention_mask=attention_mask,
-                        mm_token_type_ids=mm_token_type_ids,
+                        # mm_token_type_ids=mm_token_type_ids,
                         past_key_values=past_key_values,
                         logits_to_keep=1,
                     ).past_key_values
 
     print(f"{total=}")
     print(f"{past_key_values.get_seq_length()=}")
+    prefill_length = past_key_values.get_seq_length()
 
     messages = [
         {
             "role": "user",
-            "content": [{"type": "text", "text": ds[128]["question"]}],
+            # "content": [{"type": "text", "text": ds[128]["question"]}],
+            "content": ds[128]["question"],
         },
     ]
     rich.print(ds[128])
@@ -104,13 +121,13 @@ def main():
         ],
         dim=1,
     )
-    inputs["mm_token_type_ids"] = torch.cat(
-        [
-            torch.zeros(1, past_key_values.get_seq_length(), dtype=torch.long),
-            inputs["mm_token_type_ids"],
-        ],
-        dim=1,
-    )
+    # inputs["mm_token_type_ids"] = torch.cat(
+    #     [
+    #         torch.zeros(1, past_key_values.get_seq_length(), dtype=torch.long),
+    #         inputs["mm_token_type_ids"],
+    #     ],
+    #     dim=1,
+    # )
     inputs = inputs.to(model.device)
 
     outputs = model.generate(
@@ -119,16 +136,19 @@ def main():
     print(processor.decode(outputs[0][inputs["input_ids"].shape[-1] :]))
     print(f"{past_key_values.get_seq_length()=}")
 
-    # for layer in past_key_values.layers:
-    #    if isinstance(layer, CacheLayerMixin):
-    #        assert layer.keys is not None and layer.values is not None
-    #        rich.print(layer.keys.shape, layer.values.shape)
-    #    if isinstance(layer, LinearAttentionCacheLayerMixin):
-    #        assert layer.conv_states is not None and layer.recurrent_states is not None
-    #        rich.print(layer.conv_states.shape, layer.recurrent_states.shape)
-    # for layer_idx, queries in past_key_values.query_states.items():
-    #    rich.print(f"layer {layer_idx}: {len(queries)} query snapshots, shape {queries[0].shape}")
+    tensors = [{
+        "prefill_length": torch.tensor(prefill_length),
+        "queries": past_key_values.queries[i],
+        "keys": past_key_values.layers[i].keys,
+        "values": past_key_values.layers[i].values,
+    } for i in range(3, len(past_key_values.layers), 4)]
 
+
+    for i, tensor_dict in enumerate(tensors):
+        save_file(
+            tensor_dict,
+            f"layer_{i}_tensors.safetensors",
+        )
 
 if __name__ == "__main__":
     main()
